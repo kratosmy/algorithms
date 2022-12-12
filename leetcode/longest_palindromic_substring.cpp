faaeacd03ff0d682d039a876745399c818ef07a9
fakeryanwu <fakeryanwu@tencent.com>
2022-12-12 23:49:29 +0800

    add comment

--- leetcode/longest_palindromic_substring.cpp
+++ leetcode/longest_palindromic_substring.cpp
@@ -7,11 +7,11 @@ public:
     string longestPalindrome(string s) {
         string res;
         for (int i = 0; i < s.size(); i++) {
-            int l = i - 1, r = i + 1;
+            int l = i - 1, r = i + 1; // odd
             while (l >= 0 && r <= s.size() - 1 && s[l] == s[r]) l--, r++;
             if (res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
 
-            l = i, r = i + 1;
+            l = i, r = i + 1; // even
             while (l >= 0 && r <= s.size() - 1 && s[l] == s[r]) l--, r++;
             if (res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
         }
