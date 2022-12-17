af013d688d34794a54466baced27b7012c74fee7
fakeryanwu <fakeryanwu@tencent.com>
2022-12-17 22:45:43 +0800

    add q9

---
+++ leetcode/palindrome_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return 0;
+
+        int y = x;
+        long long res = 0;
+        while (x) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res == y;
+    }
+};
