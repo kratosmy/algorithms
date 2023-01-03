5ffdbd9e9bd99e0a1afedea0bb3732019b35cf99
fakeryanwu <fakeryanwu@tencent.com>
2023-01-03 22:34:31 +0800

    add 12/13

---
+++ leetcode/roman_to_integer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> hash;
+        int res = 0;
+        hash['I'] = 1, hash['V'] = 5;
+        hash['X'] = 10, hash['L'] = 50;
+        hash['C'] = 100, hash['D'] = 500;
+        hash['M'] = 1000;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && hash[s[i]] < hash[s[i + 1]]) {
+                res -= hash[s[i]];
+            } else {
+                res += hash[s[i]];
+            }
+        }
+        return res;
+    }
+};
