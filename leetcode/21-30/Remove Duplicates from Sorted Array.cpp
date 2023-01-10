f1eb12ac8246fc224eea935d99554f7d10e94016
fakeryanwu <fakeryanwu@tencent.com>
2023-01-10 22:06:16 +0800

    add 26/27

---
+++ leetcode/21-30/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!i || nums[i] != nums[i - 1]) nums[k++] = nums[i];
+        }
+        return k;
+    }
+};
