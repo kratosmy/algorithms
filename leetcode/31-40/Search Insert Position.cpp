4f97e148a93814e583afc01e933d854b8b27beb8
lov3camille <lov3camille@gmail.com>
2023-01-19 21:54:25 +0800

    add q35

---
+++ leetcode/31-40/Search Insert Position.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+
+        return l;
+    }
+};
