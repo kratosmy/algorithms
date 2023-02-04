c5908c477cca5490ed8f93b0b317af00968bc4ea
fakeryanwu <fakeryanwu@tencent.com>
2023-02-05 00:06:55 +0800

    add 45

---
+++ leetcode/41-50/Jump Game II.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n);
+
+        for (int i = 1, j = 0; i < n; i++) {
+            while (j + nums[j] < i) j++;
+            f[i] = f[j] + 1;
+        }
+
+        return f[n - 1];
+    }
+};
