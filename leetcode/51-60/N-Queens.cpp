1aa7057b0fba30f3eb775dc1c3372a3b509329bf
fakeryanwu <fakeryanwu@tencent.com>
2023-02-13 22:32:45 +0800

    add 51/52

---
+++ leetcode/51-60/N-Queens.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> col, dg, rdg;
+    vector<string> path;
+    vector<vector<string>> ans;
+    int n;
+
+    vector<vector<string>> solveNQueens(int _n) {
+        n = _n;
+        col = vector<bool>(n);
+        dg = rdg = vector<bool>(n * 2);
+        path = vector<string>(n, string(n, '.'));
+
+        dfs(0);
+        return ans;
+    }
+
+    void dfs(int u) {
+        if (u == n) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!col[i] && !dg[u - i + n] && !rdg[u + i]) {
+                col[i] = dg[u - i + n] = rdg[u + i] = true;
+                path[u][i] = 'Q';
+                dfs(u + 1);
+                path[u][i] = '.';
+                col[i] = dg[u - i + n] = rdg[u + i] = false;
+            }
+        }
+    }
+};
