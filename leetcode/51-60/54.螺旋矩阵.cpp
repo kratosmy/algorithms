008ff5735dfe360665220766aabe9bdeb6b3e5a9
fakeryanwu <fakeryanwu@tencent.com>
2023-02-15 23:23:36 +0800

    add 54

---
+++ leetcode/51-60/54.螺旋矩阵.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=cpp
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int n = matrix.size();
+        if (!n) return res;
+        int m = matrix[0].size();
+        vector<vector<bool>> st(n, vector<bool>(m));
+
+        int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+        for (int i = 0, x = 0, y = 0, d = 0; i < n * m; i++) {
+            res.push_back(matrix[x][y]);
+            st[x][y] = true;
+
+            int a = x + dx[d], b = y + dy[d];
+            if (a >= n || a < 0 || b >= m || b < 0 || st[a][b]) {
+                d = (d + 1) % 4;
+                a = x + dx[d], b = y + dy[d];
+            }
+            x = a, y = b;
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
