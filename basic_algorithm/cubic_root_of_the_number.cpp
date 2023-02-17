f0cc62c7322e6e13d0fe5bb9b403928aa10d2c26
fakeryanwu <fakeryanwu@tencent.com>
2023-02-17 22:18:06 +0800

    add cubic root of number

---
+++ basic_algorithm/cubic_root_of_the_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+double n;
+
+int main() {
+    cin >> n;
+
+    double l = -10000, r = 10000;
+
+    while (r - l > 1e-8) {
+        double mid = (l + r) / 2;
+
+        if (mid * mid * mid >= n) {
+            r = mid;
+        } else {
+            l = mid;
+        }
+    }
+
+    printf("%lf\n", l);
+}
