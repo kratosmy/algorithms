d31c6bf44e72040fe6ab2f20592ab4cbcddf4222
fakeryanwu <fakeryanwu@tencent.com>
2023-03-14 23:23:01 +0800

    add 67

---
+++ leetcode/61-70/67.二进制求和.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+
+        string res;
+        for (int i = 0, t = 0; i < a.size() || i < b.size() || t; i ++) {
+            if (i < a.size()) t += a[i] - '0';
+            if (i < b.size()) t += b[i] - '0';
+            res += to_string(t % 2);
+            t /= 2;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
