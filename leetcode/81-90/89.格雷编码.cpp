9bf8e0d651f4cf35e4e67a261e990b85d236ddae
fakeryanwu <fakeryanwu@tencent.com>
2023-04-14 22:33:13 +0800

    add 88/89

---
+++ leetcode/81-90/89.格雷编码.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res(1, 0);
+        while (n --) {
+            for (int i = res.size() - 1; i >= 0; i --) {
+                res[i] *= 2;
+                res.push_back(res[i] + 1);
+            }
+        }
+        return res;
+    }
+};
+
