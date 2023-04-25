2cbabb0c6eedd678c5f28933146496bfbb64dda2
lov3camille <lov3camille@gmail.com>
2023-04-25 21:22:23 +0800

    add 103/104

---
+++ leetcode/101-200/103.二叉树的锯齿形层序遍历.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+
+        int cnt = 0;
+        while (q.size()) {
+            vector<int> level;
+            int len = q.size();
+            while (len --) {
+                auto t = q.front();
+                q.pop();
+                level.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            if (++ cnt % 2 == 0) reverse(level.begin(), level.end());
+            res.push_back(level);
+        }
+        return res;
+    }
+};
+
